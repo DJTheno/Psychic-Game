8f0a58e4661a0f6344b13f9221f19c9d588ea97a
Dylan Theno <djtheno@gmail.com>
2018-12-05 23:02:57 -0600

    submission1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,34 @@
 //this is an array declaration//
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var lettersGuessed = [];
-var computerChoices = [];
 //computer random selection//
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)]; 
 
-console.log(computerChoices);
-
-document.onkeypress = function(event) {
-    var userGuess = event.key;
+console.log(computerChoice);
+//makes everything happen when key is pressed//
 
 
-if(userGuess === computerChoice){
-    wins++;
-}
-else{
-    guesses--;
-}
+document.onkeypress = function (event) {
+    var lettersGuessed = event.key;
 
-if(guesses === 0){
-    losses++
-}
+    //if else statements telling what happens when keys are pressed//
+    if (lettersGuessed === computerChoice) {
+        wins++;
+    }
+    else {
+        guessesLeft--;
+    }
 
+    if (lettersGuessed === 0) {
+        losses++
+    }
+    //this is the return of the html to my page//
     document.getElementById("wins").innerHTML = "wins: " + wins;
     document.getElementById("losses").innerHTML = "losses: " + losses;
-    document.getElementById("guessesLeft").innerHTML = "guessesLeft: " + 9;
-    document.getElementById("Guesses").innerHTML = "Guesses:" + Guesses 
+    document.getElementById("guessesLeft").innerHTML = "guessesLeft: " + guessesLeft;
+    document.getElementById("lettersGuessed").innerHTML = "lettersGuessed:" + lettersGuessed;
+    document.getElementById("computerChoices").innerHTML = "computerChoices:" + computerChoice;
 }
