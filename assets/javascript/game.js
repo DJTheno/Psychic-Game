26584085f271f26a95b54057892e15f9022223bd
Dylan Theno <djtheno@gmail.com>
2018-12-05 18:33:26 -0600

    game1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
+//this is an array declaration//
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var lettersGuessed = [];
-var computerChoice = [];
-
+var computerChoices = [];
+//computer random selection//
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 console.log(computerChoices);
 
-document.onkeypress = fuction(event) ;
+document.onkeypress = function(event) {
     var userGuess = event.key;
 
 
@@ -26,6 +27,6 @@ if(guesses === 0){
 
     document.getElementById("wins").innerHTML = "wins: " + wins;
     document.getElementById("losses").innerHTML = "losses: " + losses;
-    document.getElementById("guessesLeft").innerHTML = "guessesLeft: " + guessesLeft;
+    document.getElementById("guessesLeft").innerHTML = "guessesLeft: " + 9;
     document.getElementById("Guesses").innerHTML = "Guesses:" + Guesses 
-
+}
